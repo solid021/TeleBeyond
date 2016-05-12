306983d533eba5e9f3187061b9070acdea6bf0b2
solid021 <solid.in89@yahoo.com>
2016-05-12 18:20:11 +0430

    little bugs in launch.sh fixed :D

--- launch.sh
+++ launch.sh
@@ -111,5 +111,5 @@ else
     exit 1
   fi
 
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/beyond.bot.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/beyondbot.lua -l 1 -E $@
 fi
